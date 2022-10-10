8a0049d8a37989408c95269eab50913e3a1b3f6c
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2022-10-10 11:00:32 +0700

    Initial commit
    Initial commit, maintained by me.

---
+++ app/src/main/java/com/if3b/testing/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.if3b.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setTitle("Layout Dua");
+    }
+}
